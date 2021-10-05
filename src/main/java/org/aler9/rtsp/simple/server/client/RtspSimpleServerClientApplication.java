094fedbcfb4bdf030b1b20d1932278d9ce5e3cdf
Daniel Udatny <daniel.udatny@rosa.com>
2021-10-05 06:55:46 +0200

    initial commit

---
+++ src/main/java/org/aler9/rtsp/simple/server/client/RtspSimpleServerClientApplication.java
@@ -0,0 +1,13 @@
+package org.aler9.rtsp.simple.server.client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RtspSimpleServerClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RtspSimpleServerClientApplication.class, args);
+	}
+
+}
