311fd603cd99f91dd026f70eafecdeda1607d095
Daniel Udatny <daniel.udatny@rosa.com>
2021-10-05 13:09:27 +0200

    fixes

--- src/main/java/org/aler9/rtsp/simple/server/client/service/RtspSimpleServerApiWrapperServiceProperties.java
+++ src/main/java/org/aler9/rtsp/simple/server/client/service/RtspSimpleServerApiWrapperServiceProperties.java
@@ -1,13 +1,22 @@
 package org.aler9.rtsp.simple.server.client.service;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
 @ConfigurationProperties("rtspsimpleserverclient")
-public class ServiceProperties {
+public class RtspSimpleServerApiWrapperServiceProperties {
 
+    final static Logger LOGGER = LoggerFactory.getLogger(RtspSimpleServerApiWrapperServiceProperties.class);
+
+    private final static String DEFAULT_RTSP_SERVER_BASEADDRESS = "http://localhost:9997";
     private String serverBaseurl;
 
     public String getServerBaseurl() {
+	if (serverBaseurl == null) {
+	    LOGGER.warn("you didnt specify the rtspsimpleserverclient.serverBaseurl property, using default.");
+	    return DEFAULT_RTSP_SERVER_BASEADDRESS;
+	}
 	return serverBaseurl;
     }
 
